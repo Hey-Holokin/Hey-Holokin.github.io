9ed90b543c9e333e9a9cebc35f023db7ff0dc1b3
Holokin <UnityParadox@gmail.com>
2021-05-31 20:40:16 -0500

    Implement debug mode

--- karma.js
+++ karma.js
@@ -14,7 +14,13 @@ let exitMessage;
 let awaitInput;
 let error;
 let terminate;
-let interpretKarma = function() {
+let outputString;
+let addOutput;
+let interpretKarma = function(debug = false) {
+	//Disable input
+	document.getElementById("karma-in").disabled = true;
+	document.getElementById("execute").disabled = true;
+	document.getElementById("debug").disabled = true;
 	//Set default values for Karma core
 	input = document.getElementById("karma-in").value.split("\n");
 	output = document.getElementById("karma-out");
@@ -27,175 +33,186 @@ let interpretKarma = function() {
 	awaitInput = false;
 	error = false;
 	terminate = false;
+	outputString = [];
 	for (let i=0; i<input.length; i++) {
 		gridPosList.push(0);
 	}
 	
 	//Enter loop
-	while (gridPosList[gridLine] < input[gridLine].length && !awaitInput) {
-		let inputChar = input[gridLine][gridPosList[gridLine]]
-		
-		readKarmaCommand(inputChar);
-		
-		if (terminate || awaitInput) break;
-	}
+	let inputChar = input[gridLine][gridPosList[gridLine]]
+	if (debug) outputString = ["Press space to advance.", "Stack:", "Deque:", "", ""];
+	readKarmaCommand(inputChar, debug);
 };
-let readKarmaCommand = function(inputChar) {
+let readKarmaCommand = function(inputChar, debug) {
+	if (debug) console.log("Reading command");
 	let pNum = 0;
 	switch (inputChar) {
 		//Operands [COMPLETE]
 		case '+': {	//Pop A, pop B, push A+B
 			if (stack.length < 2) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1] + stack[stack.length-2];
 			stack.pop();
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '-': {	//Pop A, pop B, push A-B
 			if (stack.length < 2) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1] - stack[stack.length-2];
 			stack.pop();
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '*': {	//Pop A, pop B, push A*B
 			if (stack.length < 2) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1] * stack[stack.length-2];
 			stack.pop();
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '/': {	//Pop A, pop B, push A/B
 			if (stack.length < 2) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = Math.floor(stack[stack.length-1] / stack[stack.length-2]);
 			stack.pop();
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '%': {	//Pop A, pop B, push A%B
 			if (stack.length < 2) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1] % stack[stack.length-2];
 			stack.pop();
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '&': {	//Pop A, pop B, push A&B
 			if (stack.length < 2) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1] & stack[stack.length-2];
 			stack.pop();
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '|': {	//Pop A, pop B, push A|B
 			if (stack.length < 2) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1] | stack[stack.length-2];
 			stack.pop();
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '^': {	//Pop A, pop B, push A^B
 			if (stack.length < 2) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1] ^ stack[stack.length-2];
 			stack.pop();
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '~': {	//Pop A, push ~A
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = ~stack[stack.length-1];
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '!': {	//Pop A, push !A
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = !stack[stack.length-1];
 			stack.pop();
 			stack.push(pNum);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		//Logic [COMPLETE]
 		case '=': {	//Pop A, push 1 if A is equal to front of deque, else push 0
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1];
 			stack.pop();
 			if (pNum == deque[0]) stack.push(1);
 			else stack.push(0);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '>': {	//Pop A, push 1 if A is greater than front of deque, else push 0
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1];
 			stack.pop();
 			if (pNum == deque[0]) stack.push(1);
 			else stack.push(0);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '@': {	//Pop A, skip next command if A is not 1
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			pNum = stack[stack.length-1];
 			stack.pop();
 			if (pNum != 1) gridPosList[gridLine]++;
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		//Stack/Deque [COMPLETE]
@@ -210,59 +227,83 @@ let readKarmaCommand = function(inputChar) {
 		case '8':	//Push digit 8
 		case '9': {	//Push digit 9
 			stack.push(parseInt(inputChar));
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '}': {	//Pop A, insert A at front of deque
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			deque.unshift(stack[stack.length-1]);
 			stack.pop();
+			if (debug) {
+				outputString[1] = "Stack: " + stack.join(" ");
+				outputString[2] = "Deque: " + deque.join(" ");
+			}
 			break;
 		}
 		case '{': {	//Remove A from front of deque, push A
 			if (deque.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Deque underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Deque underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			stack.push(deque[0])
 			deque.shift();
+			if (debug) {
+				outputString[1] = "Stack: " + stack.join(" ");
+				outputString[2] = "Deque: " + deque.join(" ");
+			}
 			break;
 		}
 		case '[': {	//Pop A, insert A at back of deque
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			deque.push(stack[stack.length-1]);
 			stack.pop();
+			if (debug) {
+				outputString[1] = "Stack: " + stack.join(" ");
+				outputString[2] = "Deque: " + deque.join(" ");
+			}
 			break;
 		}
 		case ']': {	//Remove A from back of deque, push A
 			if (deque.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Deque underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Deque underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			stack.push(deque[deque.length-1]);
 			deque.pop();
+			if (debug) {
+				outputString[1] = "Stack: " + stack.join(" ");
+				outputString[2] = "Deque: " + deque.join(" ");
+			}
 			break;
 		}
 		case '#': {	//Pop A
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
 			stack.pop();
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case '\\':{	//Clone top of stack
+			if (stack.length == 0) {
+				terminate = true;
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
+				break;
+			}
 			stack.push(stack[stack.length-1]);
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		//I/O [COMPLETE]
@@ -273,21 +314,25 @@ let readKarmaCommand = function(inputChar) {
 		case ':': {	//Pop A, print A as character
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
-			output.innerHTML += String.fromCharCode(stack[stack.length-1]);
+			if (debug) outputString[3] += String.fromCharCode(stack[stack.length-1]);
+			else output.innerHTML += String.fromCharCode(stack[stack.length-1]);
 			stack.pop();
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		case ';': {	//Pop A, print A as number
 			if (stack.length == 0) {
 				terminate = true;
-				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ".";
+				exitMessage = "ERROR: Stack underflow. Program terminated at line " + gridLine + ", character " + gridPosList[gridLine] + ".";
 				break;
 			}
-			output.innerHTML += stack[stack.length-1];
+			if (debug) outputString[3] += String.fromCharCode(stack[stack.length-1]);
+			else output.innerHTML += stack[stack.length-1];
 			stack.pop();
+			if (debug) outputString[1] = "Stack: " + stack.join(" ");
 			break;
 		}
 		//Jumps [COMPLETE]
@@ -337,7 +382,10 @@ let readKarmaCommand = function(inputChar) {
 	}
 	
 	if (terminate) {
-		output.innerHTML += '\n\n<p style="color: red;">' + exitMessage + "</p>";
+		output.innerHTML += '\n\n<span style="color: var(--karma-error-red);">' + exitMessage + "</span>";
+		document.getElementById("karma-in").disabled = false;
+		document.getElementById("execute").disabled = false;
+		document.getElementById("debug").disabled = false;
 	} else {
 		gridPosList[gridLine]++;
 		
@@ -345,16 +393,39 @@ let readKarmaCommand = function(inputChar) {
 			if (awaitInput) {
 				let getch = function(e) {
 					stack.push(e.keyCode);
-					readKarmaCommand(input[gridLine][gridPosList[gridLine]]);
-					document.removeEventListener("keypress", getch);
+					if (debug) {
+						outputString[1] = "Stack: " + stack.join(" ");
+						document.addEventListener("keydown", waitSpace);
+					} else {
+						readKarmaCommand(input[gridLine][gridPosList[gridLine]], true);
+					}
 					awaitInput = false;
+					document.removeEventListener("keypress", getch);
 				}
 				document.addEventListener("keypress", getch);
 			} else {
-				readKarmaCommand(input[gridLine][gridPosList[gridLine]]);
+				if (!debug) readKarmaCommand(input[gridLine][gridPosList[gridLine]]);
+				else {
+					document.addEventListener("keydown", waitSpace);
+				}
 			}
 		} else {
-			output.innerHTML += '\n\n<p style="color: blue;">Program exit. Program successfully executed, line ' + gridLine + ".";
+			terminate = true;
+			exitMessage = '\n\n<span style="color: var(--karma-blue);">Program exit. Program successfully executed, line ' + gridLine + ".</span>"
+			if (debug) outputString[3] += exitMessage;
+			else output.innerHTML += exitMessage;
+			document.getElementById("karma-in").disabled = false;
+			document.getElementById("execute").disabled = false;
+			document.getElementById("debug").disabled = false;
 		}
+	}
+	
+	if (debug) output.innerHTML = outputString.join("\n");
+}
+let waitSpace = function(e) {
+	if (e.keyCode == 32) {
+		document.removeEventListener("keydown", waitSpace);
+		console.log("Success");
+		readKarmaCommand(input[gridLine][gridPosList[gridLine]], true);
 	}
 }
